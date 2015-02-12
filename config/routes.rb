c3661aa0f2d8f472645020480434cd04fdb8c7e3
Adam Ouellette <adamouellette57@gmail.com>
2015-02-12 10:45:07 -0500

    Create Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/isiton'
+
+  get 'static_pages/recommend'
+
+  get 'static_pages/randomMovie'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
