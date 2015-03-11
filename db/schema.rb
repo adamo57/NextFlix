67d2d7f78b29cc9523edf814e82c1580c7fd799e
smorgasbordator <smorgasbordator@gmail.com>
2015-03-11 13:05:21 -0400

    Added some backend for admin users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311163616) do
+ActiveRecord::Schema.define(version: 20150311170204) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -26,10 +26,11 @@ ActiveRecord::Schema.define(version: 20150311163616) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
